6c4a7c68a0ecb1c56f2cca0de358cd9b5575ed37
Daniel <grunler@kth.se>
2021-04-06 19:31:33 +0200

    try markdown in comment

--- contributions/course-automation/augustjo-grunler/index.js
+++ contributions/course-automation/augustjo-grunler/index.js
@@ -112,18 +112,18 @@ function checkDeadline(deadline, timeOfSubmission) {
 
 function getWordCountVerdict(wordCount, acceptableLimit, remarkableLimit) {
   let wc = parseInt(wordCount);
-  let verdict = (wc < acceptableLimit) ? 'no it is not': (wc >= remarkableLimit ? 'yes it is remarkable!' : 'yes it is');
+  let verdict = (wc < acceptableLimit) ? 'no': (wc >= remarkableLimit ? 'yes, remarkable!' : 'yes');
   return verdict;
 }
 
 function createCommentBody(filename, path, wc, verdict, timeOfSubmission, deadline ) {
-  let comment = 'Checking word count and time of submission for feedback given\n';
-  let fileString = `File checked: ${filename}. (${path}) \n`;
-  let wordCountString = `Feedback is substantiated: ${verdict} (${wc} words) \n`;
+  let comment = 'Checking basic feedback requirements. \n';
+  let fileString = `**File checked**: ${filename}. (${path}) \n`;
+  let wordCountString = `**Passes word limit**: ${verdict} (${wc} words.) \n`;
   //check time of submission
   let timeApproved = checkDeadline(deadline,timeOfSubmission)
-  let timeCheckString = timeApproved? `The submission is delivered before the deadline` : `the submission is delivered after the deadline`
-  timeCheckString += ` (submitted at: ${timeOfSubmission.toString()}) \n` 
+  let timeCheckString = timeApproved? `The feedback was submitted in time` : `The feedback was submitted after the deadline`
+  timeCheckString += ` (submitted at: ${timeOfSubmission.toString()}.) \n` 
   comment = comment + fileString + wordCountString +  timeCheckString;
   return comment;
 }
