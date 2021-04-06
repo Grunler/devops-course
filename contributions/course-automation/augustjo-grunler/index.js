21e55e87becfcdca0b88655f3df809ab7aeb48c1
Daniel <grunler@kth.se>
2021-04-06 20:44:39 +0200

    cleanup, remove log

--- contributions/course-automation/augustjo-grunler/index.js
+++ contributions/course-automation/augustjo-grunler/index.js
@@ -5,49 +5,41 @@ var atob = require('atob');
 
 async function main() {
   try {
-    // Inputs
-    const token = core.getInput('repo-token');
+    // Constants
+    const token = core.getInput('repo-token')
     const minimalWordCountLimit = core.getInput('minimal-wordcount')
     const remarkableWordCountLimit = core.getInput('remarkable-wordcount')
     const deadline = new Date(core.getInput('deadline'))
-    const octokit = github.getOctokit(token);
+    const octokit = github.getOctokit(token)
     const repoName = github.context.repo.repo
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    const owner = github.context.payload.repository.owner.login;
+    const owner = github.context.payload.repository.owner.login
     const branch = github.context.payload.pull_request.head.ref
 
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // extract required information from payload from github
-    console.log(`The owner of the repo is ${owner}`)
-    console.log(`the branch is: ${branch}`)
     var issue_number = github.context.payload.pull_request._links.issue.href.split("/")
     issue_number = issue_number[issue_number.length-1]
-    console.log(`Pull request to: ${repoName}`)
     let timeOfSubmission = github.context.payload.pull_request.head.repo.pushed_at
     timeOfSubmission = new Date(timeOfSubmission)
 
     // get changed files from pullrequest
     var files = await getChangedfiles(owner, repoName, issue_number, octokit)
+    //Find README
     files = files.filter(file => file.filename.includes('README.md'))
     files = files.filter(file => file.filename.includes('feedback'))
-    console.log(files)
+
+    //Get the README's directory
     let dir = files[0].filename.split('/')
-    const reducer = (accumulator, word) => accumulator + "/" + word;
+    const reducer = (accumulator, word) => accumulator + "/" + word
     dir.pop()
     dir = dir.reduce(reducer)
-    console.log(dir)
   
     // Extract The readme file with the feedback from the correct directory
     var file = await getReadme(octokit,owner,repoName,dir,branch)
     const path = file.path
-    console.log(`path is: ${path}`)
-    core.setOutput("readme_path", path)
-    var rawText = atob(file.content)
-    console.log(`This is the rawtext of the readme: ${rawText}`)
-    
-    //get wordcount of feedback
-    var wordCount = getMDwordCount(rawText)
+    var markdown = atob(file.content)
+
+    //Calculate feedback word count
+    var wordCount = getMDwordCount(markdown)
     var wordCountReached = getWordCountVerdict(wordCount,minimalWordCountLimit,remarkableWordCountLimit)
 
   
@@ -62,10 +54,8 @@ async function main() {
 
 function getMDwordCount(string) {
   str = string.replace(/([#\*>\+|/_@±/\[\]\\{}<±\-`]+)/g,"");
-  console.log(`String after first replace: ${str} `);
   str = str.replace(/(\s)+/g," ");
   str = str.trim();
-  console.log(`String: ${str} `);
   return str.split(" ").length;
 }
 
@@ -137,5 +127,4 @@ async function buildAndPostComment(issue_number, message, octokit) {
   });
 }
 
-// run main function
 main()
